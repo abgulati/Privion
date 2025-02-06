905c2f33c60887b067d38a02cb650862f0388abc
AbheekG <abheekg@hotmail.com>
2025-02-05 20:44:05 -0800

    Awaiting retrieval of full user-query from chat-history.db when regenerating response

--- web_app/static/js/dom-loader.js
+++ web_app/static/js/dom-loader.js
@@ -19,22 +19,23 @@ function initializeScrollDownButton() {
 }
 
 function initializeRegenerateResponseButton() {
-    document.getElementById('chat-area').addEventListener('click', function(e) {
+    document.getElementById('chat-area').addEventListener('click', async function(e) {
         if (e.target.classList.contains('regenerate-option')) {
             const userMessageDiv = e.target.closest('.user-message');
-            const {streamSessionId, sequenceId} = prepareAttributeForUserMessage(userMessageDiv);
+            const {streamSessionId, sequenceId} = await prepareAttributeForUserMessage(userMessageDiv);
             requestFormattedPrompt(true, false, false, streamSessionId, sequenceId);    // Request the formatted prompt
             deleteChatAreaElements(userMessageDiv.nextElementSibling); // Delete subsequent user messages and response containers
         } else if (e.target.classList.contains('regenerate-with-citations-enabled-option')) {
             const userMessageDiv = e.target.closest('.user-message');
-            const {streamSessionId, sequenceId} = prepareAttributeForUserMessage(userMessageDiv);
+            const {streamSessionId, sequenceId} = await prepareAttributeForUserMessage(userMessageDiv);
             requestFormattedPrompt(true, true, false, streamSessionId, sequenceId);    // Request the formatted prompt
             deleteChatAreaElements(userMessageDiv.nextElementSibling); // Delete subsequent user messages and response containers
         } else if (e.target.classList.contains('regenerate-with-citations-disabled-option')) {
             const userMessageDiv = e.target.closest('.user-message');
-            const {streamSessionId, sequenceId} = prepareAttributeForUserMessage(userMessageDiv);
+            const {streamSessionId, sequenceId} = await prepareAttributeForUserMessage(userMessageDiv);
             requestFormattedPrompt(true, false, true, streamSessionId, sequenceId);    // Request the formatted prompt
             deleteChatAreaElements(userMessageDiv.nextElementSibling); // Delete subsequent user messages and response containers
+
         } else if (e.target.classList.contains('delete-option')) {
             const userMessageDiv = e.target.closest('.user-message');
             const chatId = userMessageDiv.getAttribute('data-chat-id');
