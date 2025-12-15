78eac9dde0e8609b12bf545a298e54596020d98e
Cameron Soviero <camsoviero@att.net>
2025-12-15 12:34:33 -0600

    Added Kasa smart bulb support with a hard coded address for Kasa smart bulb. Expanded on LLM tool schema to allow new property_def fields for minimum and maximum. This mitigates the sort of guessing game that was taking place during the model tool care where the model was often incorrectly guessing or assuming that it could set a field value beyond the allowed maximum

--- web_app/ha_device_modules/kasa_plug.py
+++ web_app/ha_device_modules/kasa_plug.py
@@ -1,8 +1,5 @@
-import sys
-import asyncio
-from contextlib import suppress
-
-from kasa import SmartPlug as KasaSmartPlug
+# from kasa import SmartPlug as KasaSmartPlug
+from kasa.iot import IotPlug as KasaSmartPlug
 
 KASA_PLUG_IP_ADDR = "192.168.0.68"
 
@@ -12,14 +9,14 @@ async def kasa_connect():
         await dev.update()
         return dev
     except Exception as e:
-        return {"success": False, "message": f"Error turning on Kasa plug: {e}"}
+        return {"success": False, "message": f"Error connecting to Kasa plug: {e}"}
     
 async def kasa_disconnect(dev):
     try:
         await dev.protocol.close()
         dev = None
     except Exception as e:
-        return {"success": False, "message": f"Error turning on Kasa plug: {e}"}
+        return {"success": False, "message": f"Error disconnecting from Kasa plug: {e}"}
 
 async def plug_turn_on_handler():
     try:
@@ -27,10 +24,9 @@ async def plug_turn_on_handler():
         await dev.turn_on()
         print("Turned ON")
         await kasa_disconnect(dev)
-        return {"success": True, "message": "Light turn-on command sent to Kasa device."}
+        return {"success": True, "message": "Turn-on command sent to Kasa plug."}
     except Exception as e:
         return {"success": False, "message": f"Error turning on Kasa plug: {e}"}
-
 
 async def plug_turn_off_handler():
     try:
@@ -38,6 +34,7 @@ async def plug_turn_off_handler():
         await dev.turn_off()
         print("Turned OFF")
         await kasa_disconnect(dev)
-        return {"success": True, "message": "Light turn-off command sent to Kasa device."}
+        return {"success": True, "message": "Turn-off command sent to Kasa plug."}
     except Exception as e:
         return {"success": False, "message": f"Error turning off Kasa plug: {e}"}
+    
